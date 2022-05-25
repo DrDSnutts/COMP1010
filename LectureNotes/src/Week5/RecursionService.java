be623fdf6c23ff4ff8eb3d964434cae704365cb4
DrDSnutts <dominic@walkham.com>
2022-05-25 11:41:42 +1000

    change

--- LectureNotes/src/Week5/RecursionService.java
+++ LectureNotes/src/Week5/RecursionService.java
@@ -1,13 +1,26 @@
 package Week5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ListIterator;
+
 public class RecursionService {
 
 	public static void main(String[] args) {
-		 
+		ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(10,70,20,90));
+		
+		System.out.println(list);
+		
+		list.add(1,60);
+		System.out.println(list);
+		
+		list.remove(70);
+		System.out.println(list);
+		 }
     }
 
 	
-}
+
 
 	
 
